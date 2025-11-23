23d1ce7e13c45bb23d5882ed876fa06d5d4d85af
ramtechnow20 <bvhss20@gmail.com>
2025-11-23 13:24:56 +0530

    small_changes

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -4,7 +4,7 @@ import { Popular } from '../Components/Popular/Popular'
 import { Offers } from '../Components/Offers/Offers'
 import { NewCollection } from '../Components/NewCollections/NewCollection'
 import { NewsLetter } from '../Components/NewsLetter/NewsLetter'
-
+// new changes
 export const Shop = () => {
   return (
     <div>
