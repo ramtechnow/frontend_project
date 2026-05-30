fc59bd47e400d651fc26b35eb33fe69e1ceec6f4
ramtechnow20 <bvhss20@gmail.com>
2026-05-30 13:22:54 +0530

    test_1

--- frontend_project/src/config.js
+++ frontend_project/src/config.js
@@ -2,4 +2,4 @@
 // Change the backend URL below or set REACT_APP_BACKEND_URL in .env
 
 // export const BACKEND_URL = "http://10.151.98.193:4000";
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://my-ecommerce-api.onrender.com";
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "https://frontend-project-jucn.onrender.com";
